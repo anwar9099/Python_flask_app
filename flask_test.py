b0ec417631633d9ebdd208c8ac7d94c5a9aedc69
anwar mohamed <anwar909@hotmail.co.uk>
2025-10-06 09:50:41 +0100

    Add deploy steps and creates and updates docker image using secrets

--- flask_test.py
+++ flask_test.py
@@ -1,4 +1,4 @@
-from flaskpp import app #imports my app;icatin from flaskpp
+from flaskpp import app #imports my application from flaskpp
 def test_route(): 
     response  = app.test_client().get('/')  #test_client is a test browser and its gets my route "/"
     assert response.status_code == 200  #maks sure route returns 200 okay response
